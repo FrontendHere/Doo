510cef99276c554185b3c83412d396fb80c7ad12
iamfrontend <iamfrontend>
2018-10-09 17:12:36 +0500

    Services - step 0

--- src/first-monitor/first-monitor.js
+++ src/first-monitor/first-monitor.js
@@ -1,3 +1,9 @@
 ;(function () {
-	$('.first-monitor-i__section').css('height', $('.first-monitor-i').innerHeight() - $('.header-i').innerHeight());
+	$(window).resize(function(){
+		if(minWidth992.matches) {
+		    $('.first-monitor-i__section').css('height', 'auto');
+		} else {
+		    $('.first-monitor-i__section').css('height', $('.first-monitor-i').innerHeight() - $('.header-i').innerHeight());
+		}		
+	});
 }());
--- src/script.js
+++ src/script.js
@@ -1,4 +1,10 @@
 ;$(document).ready(function() {
+	var minWidth992 = window.matchMedia('@media (min-width: 992px)');
+	if(minWidth992.matches) {
+	    // ширина браузера меньше, чем 992px
+	} else {
+	    // ширина браузера больше, чем 992px
+	}
 	//= first-monitor/first-monitor.js
 	//= first-monitor/header/menu/menu.js
 });
