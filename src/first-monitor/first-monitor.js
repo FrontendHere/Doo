338f5c80c24f39ddf9877a8d93617415dfc2d998
iamfrontend <iamfrontend>
2018-10-11 17:44:37 +0500

    Reliz to web

--- src/first-monitor/first-monitor.js
+++ src/first-monitor/first-monitor.js
@@ -1,9 +1,17 @@
 ;(function () {
+	if(window.matchMedia('(min-width: 992px)').matches)
+	{
+		$('.first-monitor-i__section').css('height', $('.first-monitor-i').innerHeight() - $('.header-i').innerHeight());			  
+	}
+	else{
+		$('.first-monitor-i__inner-image').css('height', $('.first-monitor-i').innerHeight());
+	}
+
 	$(window).resize(function(){
-		if(minWidth992.matches) {
-		    $('.first-monitor-i__section').css('height', 'auto');
-		} else {
-		    $('.first-monitor-i__section').css('height', $('.first-monitor-i').innerHeight() - $('.header-i').innerHeight());
+		if(minWidth992.matches) {		
+		   $('.first-monitor-i__section').css('height', $('.first-monitor-i').innerHeight() - $('.header-i').innerHeight()); 	   		    
+		} else {	
+			$('.first-monitor-i__inner-image').css('height', $('.first-monitor-i').innerHeight()); 		    
 		}		
 	});
 }());
