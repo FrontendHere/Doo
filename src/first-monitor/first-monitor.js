9ee2c1fe2d856b635d4d4b3eed57a9af66ee1fe2
IamFrontend <iamfrontend@mail.ru>
2018-10-12 14:37:55 +0500

    Release

--- src/first-monitor/first-monitor.js
+++ src/first-monitor/first-monitor.js
@@ -3,15 +3,10 @@
 	{
 		$('.first-monitor-i__section').css('height', $('.first-monitor-i').innerHeight() - $('.header-i').innerHeight());			  
 	}
-	else{
-		$('.first-monitor-i__inner-image').css('height', $('.first-monitor-i').innerHeight());
-	}
 
 	$(window).resize(function(){
 		if(minWidth992.matches) {		
 		   $('.first-monitor-i__section').css('height', $('.first-monitor-i').innerHeight() - $('.header-i').innerHeight()); 	   		    
-		} else {	
-			$('.first-monitor-i__inner-image').css('height', $('.first-monitor-i').innerHeight()); 		    
 		}		
 	});
 }());
